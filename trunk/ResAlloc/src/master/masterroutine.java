6cf9409526609647f864d904cb5920b07adefda2
yee.matthew <yee.matthew@5c4d7950-bf4d-0410-ac20-f93b0e8bf332>
2008-06-03 17:01:51 +0000

    Initial import.

---
+++ trunk/ResAlloc/src/master/masterroutine.java
@@ -0,0 +1,33 @@
+package master;
+
+import contractObject.Contract;      
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+public class masterroutine {
+	
+	public static void main(String[] args) {
+		Contract newContract = new Contract();
+		String owner = Integer.toString(newContract.getOwnership());		
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setSize(100,100);
+		shell.open();
+		
+		Label label1 = new Label(shell, SWT.BORDER);
+		label1.setText(owner);
+		label1.setSize(100,20);
+		label1.setLocation(10,10);
+	
+		Text text1 = new Text(shell, SWT.H_SCROLL);
+		text1.setTextLimit(30);
+		
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+
+	}
+
+}
